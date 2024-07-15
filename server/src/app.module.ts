ed8a1ad39554df1271ee409d115cb06d06752455
Lossteg <94453559+Lossteg@users.noreply.github.com>
2024-07-15 19:38:22 +0300

    finally fixed for real:

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,15 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { envConfigSchema } from './configuration';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({validationSchema: envConfigSchema})
+    ConfigModule.forRoot({validationSchema: envConfigSchema}),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'client'),
+    }),
 ],
   controllers: [AppController],
   providers: [AppService],
