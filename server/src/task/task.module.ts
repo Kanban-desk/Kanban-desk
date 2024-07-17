5a89fd8395f00b207e1f18a5cfcdc644a1b65623
Lossteg <94453559+Lossteg@users.noreply.github.com>
2024-07-17 12:29:31 +0300

    fix: add correct import for comment entity

--- server/src/task/task.module.ts
+++ server/src/task/task.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Status } from 'src/desk/entities/status.entity';
 import { Priority } from './entities/priority.entity';
+import Comme
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Status, Comment, Priority])],
